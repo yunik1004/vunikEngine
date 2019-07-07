db1f9086b820e611add4cc9642a86928b408fbda
yunik1004 <inkyuhak@gmail.com>
2019-07-07 23:19:27 +0900

    Add external libraries

---
+++ include/vunikEngine/init.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace vunikEngine {
+    class Init {
+    public:
+        static bool init (void);
+        static void deinit (void);
+    };
+}
---
+++ src/init.cpp
@@ -0,0 +1,11 @@
+#include "vunikEngine/init.hpp"
+
+namespace vunikEngine {
+    bool Init::init (void) {
+        return true;
+    }
+
+    void Init::deinit (void) {
+        //
+    }
+}
